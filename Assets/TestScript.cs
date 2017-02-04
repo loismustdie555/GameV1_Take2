317dbee5c336c427e792101cc1cb66f3ded2459f
loismustdie555 <555loismustdie@gmail.com>
2017-02-03 19:03:19 -0800

    Start

---
+++ Assets/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+    // Use this for initialization
+    Text test;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        test.text = Input.GetAxis("Vertical").ToString();
+    }
+}
